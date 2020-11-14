19d05afaa5dcfae740b062d4a4dc8ac824d03a14
jameslomax182 <74469095+jameslomax182@users.noreply.github.com>
2020-11-14 22:56:25 +0100

    Add files via upload

---
+++ JamesAdivino/src/jamesadivino/JamesAdivino.java
@@ -0,0 +1,27 @@
+package jamesadivino;
+
+import java.util.Scanner;
+
+public class JamesAdivino {
+
+
+    public static void main(String[] args) {
+        int n = 0,cont = 0;
+        int x = (int) (Math.random() * (50 - 10)) +10;
+        System.out.print(x);
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("\nIntroduce el valor de X: ");
+            n = sc.nextInt();
+            if (n > x) {
+                System.out.print("Te pasaste Amigo");
+            } else if (n < x){
+                System.out.print("Casi cerca ...");
+            }
+            cont++;
+        } while (n != x);
+        System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        sc.close();
+    }
+}
+
